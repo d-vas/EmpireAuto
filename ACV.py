0b40954471b410a654670aeca3657d77505316da
d-vas <dzyadekvl@gmail.com>
2023-11-29 17:21:51 -0500

    ACV in process

--- ACV.py
+++ ACV.py
@@ -3,19 +3,20 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from settings import ZIPS
+import settings
 
 
-def determine_region(pu_zip, del_zip, zips):
+'''def determine_region(pu_zip, del_zip, zips):
     for i in zips:
         if pu_zip in zips[i]:
             pu_range = zips[i]
+            print(i, ' - ', pu_range)
     for i in zips:
         if del_zip in zips[i]:
             del_range = zips[i]
-    # print(pu_range)
-    # print(del_range)
+            print(i, ' - ', del_range)
     return pu_range, del_range
-
+'''
 
 def acv_login_getting_table(login, password):
     url = 'https://transport-v1.acvauctions.com/jobs/available.php'
@@ -37,33 +38,41 @@ def acv_login_getting_table(login, password):
     table = tables[2] #res - sel elem
     table_html = table.get_attribute('outerHTML') #res - html-str
     # print(table_html)
+    return table_html
 
 
-def acv_getting_list(table_html, pu_zip_range, del_zip_range, pu_zip, del_zip):
+def acv_getting_list(table_html, pu_zip, del_zip, zips):
     df = pd.read_html(table_html)[0]
     df = df.fillna('NONE')
     df = df.iloc[5:-1]
     df.columns = df.iloc[0]
-
     df = df.iloc[1:]
     df = df.drop(columns=['NONE', 'DT', 'Date', 'Inop?', 'Address', 'Distance'])
     df.columns = ['Order ID', 'Vehicle', 'PU_City', 'PU_State', 'PU_Zip', 'DEL_City', 'DEL_State', 'DEL_Zip', 'Payout']
 
-    albany_ZIP_list = ['120', '121', '122', '123', '124', '125']
-    filtered_df = df[(df['PU_State'] == 'NY') & (df['DEL_State'] == 'NY')]
+    df['PU_Zip'] = df['PU_Zip'].astype(int)
+    df['DEL_Zip'] = df['DEL_Zip'].astype(int)
 
-    # make zip int
-    filtered_df['PU_Zip'] = filtered_df['PU_Zip'].astype(int)
-    filtered_df['DEL_Zip'] = filtered_df['DEL_Zip'].astype(int)
+    pu_range = []
+    del_range = []
 
-    # filter table by zip
-    filtered_df = filtered_df[filtered_df['PU_Zip'] in pu_zip_range]
-    filtered_df = filtered_df[filtered_df['DEL_Zip'] in del_zip_range]
+    for i in zips:
+        if pu_zip in zips[i]:
+            pu_range = zips[i]
+            # print(i, ' - ', pu_range)
+    for i in zips:
+        if del_zip in zips[i]:
+            del_range = zips[i]
+            # print(i, ' - ', del_range)
 
-    # print(filtered_df)
+    df = df[df['PU_Zip'] in pu_range]
+    df = df[df['DEL_Zip'] in del_range]
+    # filtered_df = filtered_df[filtered_df['DEL_Zip'] in del_zip_range]
 
+    print(df)
 
-determine_region(13206, 14001, ZIPS)
-# table_html = acv_login_getting_table(settings.ACV['login'], settings.ACV['pass'])
-# acv_getting_list(table_html, pu_zip, del_zip)
+# determine_region(13902, 13606, ZIPS)
+table_html = acv_login_getting_table(settings.ACV['login'], settings.ACV['pass'])
+
+acv_getting_list(table_html, pu_zip, del_zip)
 
