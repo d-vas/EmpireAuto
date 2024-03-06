e5822aa06920b096a8ac90b045bb00cc7d8a43f5
d-vas <dzyadekvl@gmail.com>
2024-03-05 23:15:10 -0500

    20240305 home 1D big success got addresses

--- 1dispatch.py
+++ 1dispatch.py
@@ -4,6 +4,9 @@ from selenium.webdriver.common.by import By
 import settings
 from SD_getting_loads import filling_sheet, open_file
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 
 
@@ -11,9 +14,7 @@ URL = 'https://www.1dispatch.com/Carrier/CarrierViewHistory?DataType=ReLoad'
 login = settings.ONE_DISP['login']
 password = settings.ONE_DISP['password']
 
-
 cookies = requests.get(URL).cookies.get_dict()
-
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
@@ -25,45 +26,42 @@ one_dispatch_list_pu = []
 one_dispatch_list_del = []
 
 
-def get_address_from_link_selenium(load_link, browser):
+def get_address_from_link_selenium(load_link):
+    # one_disp_login(login, password, load_link, browser=browser)
     browser.get(load_link)
-    pu_address = browser.find_element(By.XPATH, r'/html/body/div[1]/div[3]/section/div[5]/form/table/tbody/tr[2]/td/div[1]/table/tbody/tr[5]/td[4]')
-    # pu_address = browser.find_elements(By.CLASS_NAME, 'loadcontent')
-    # table = browser.find_elements(By.TAG_NAME, 'table')
-    # t1 = table[1].find_element(By.CLASS_NAME, 'LoadDetail')
-    # pu_address = table.find_element(By.CLASS_NAME, 'subgridhead')
-    # pu_address = browser.find_element(By.CSS_SELECTOR, '#frmLoadDetails > table > tbody > tr:nth-child(2) > td > div.LoadDetail > table > tbody > tr.loadcontent > td:nth-child(4)')
-    print(pu_address)
-    print(len(table))
-    print(table[4].text)
+    res = browser.find_element(By.CLASS_NAME, 'subgridhead')
+    load_info = res.find_element(By.CLASS_NAME, 'loadcontent ')
+    pu_loc = load_info.find_elements(By.TAG_NAME, 'td')[3].text
+    del_loc = load_info.find_elements(By.TAG_NAME, 'td')[4].text
+    return pu_loc, del_loc
 
 
-def get_address_from_link(load_link, cookies):
-    load_info = requests.get(load_link, cookies)
-    # data = load_info.
-    # print(data)
-    # "//*[@id="frmLoadDetails"]/table/tbody/tr[2]/td/div[1]/table/tbody/tr[5]/td[4]"
+def onedisp_elem_to_dict(elem): #making one load-elem to dict
+    txt = elem.text
+    lst = txt.split('\n')
 
+    dct = dict()
 
+    dct['load_id'] = lst[2].split()[2]
+    dct['total_price'] = lst[4].split()[-1]
+    dct['vins'] = lst[3]
+    dct['car_name'] = lst[5]
+           # 'pu_loc': lst[7],
+           # 'del_loc': lst[10],
+    dct['pu_del_status'] = lst[-4]
+    dct['expect_date'] = f"{lst[-3].split()[-1]} - {lst[-4]}"
+    dct['customer/broker'] = lst[0].split('Shipper: ')[-1]
+    dct['vehicles_count'] = int(lst[2].split()[4])
+    dct['load_info'] = f"{lst[2].split()[2]}\n\n{lst[3]} - {lst[5]}\n\nASSIGNED to {lst[6].split()[2] + ' ' + lst[6].split()[3]}\n\n{lst[7]} - > {lst[10]}"
 
-def one_disp_text_to_dict(text): #making one load-elem to dict
-    lst = text.split('\n')
-    dct = {'load_id': lst[2].split()[2],
-           'total_price': lst[4].split()[-1],
-           'vins': lst[3],
-           'car_name': lst[5],
-           'pu_loc': lst[7],
-           'del_loc': lst[10],
-           'pu_del_status': lst[-4],
-           'expect_date': f"{lst[-3].split()[-1]} - {lst[-4]}",
-           'customer/broker': lst[0].split('Shipper: ')[-1],
-           'vehicles_count': int(lst[2].split()[4]),
-           'load_info': f"{lst[2].split()[2]}\n\n{lst[3]} - {lst[5]}\n\nASSIGNED to {lst[6].split()[2] + ' ' + lst[6].split()[3]}\n\n{lst[7]} - > {lst[10]}",
-           'link': f""}
     if '/' in lst[6].split()[2]:
-        dct['driver'] = 'no driver assigned'
+        dct['driver'] = "no driver assigned"
     else:
         dct['driver'] = lst[6].split()[2] + ' ' + lst[6].split()[3]
+    dct['lnk'] = elem.find_element(By.TAG_NAME, 'a').get_attribute("href")
+    # print(lnk)
+    # dct['pu_loc'] = get_address_from_link_selenium(lnk)[0]
+
     return dct
 
 
@@ -74,14 +72,6 @@ def one_disp_sort_pu_del(lst, lst_pu, lst_del):
         elif l['pu_del_status'] == 'Pending Delivery':
             lst_del.append(l)
     lst.remove(l)
-
-
-def get_list_of_loads(list_of_elem, load_list):
-    for i in list_of_elem:
-        lnk = i.find_element(By.TAG_NAME, 'a').get_attribute("href")
-        dct = one_disp_text_to_dict(i.text)
-        dct['link'] = lnk
-        load_list.append(dct)
 
 
 def one_disp_login(login, password, url, browser):
@@ -108,47 +98,37 @@ def getting_all_table(login, password, browser, load_list):
     pu_del_lst.find_element(By.ID, 'btnFilterStatus').click() #click "Apply"
 
     div1 = browser.find_element(By.ID, 'GridCarrierLoadViewHistory') #whole table of loads
-    table = div1.find_elements(By.CLASS_NAME, 'grsdTbl') #list of loads-elems
-    # print(type(table))
-    '''for i in table:
-        lnk = i.find_element(By.TAG_NAME, 'a')
-        print(lnk)'''
-    get_list_of_loads(table, load_list=one_dispatch_list)
+    table = div1.find_elements(By.CLASS_NAME, 'grsdTbl') #list of loads-elements
+    return table
 
-    # for i in one_dispatch_list:
-    #     print(i)
-    # print(len(one_dispatch_list))
 
+
+    # get_list_of_loads(table, one_dispatch_list)
 
 
 if __name__ == '__main__':
 
-    get_address_from_link_selenium(
-        'https://1dispatch.com/Carrier/GetLoadDetailForCarrier?marketId=26666462&&Page=CarrierHistoryPartilaView',
-        browser=browser)
+    listofloadselements = getting_all_table(login=login, password=password, browser=browser, load_list=one_dispatch_list)
 
-    # getting_all_table(login=login, password=password, browser=browser, load_list=one_dispatch_list)
-    # print(f"total - {len(one_dispatch_list)}")
-    # one_disp_sort_pu_del(lst=one_dispatch_list, lst_pu=one_dispatch_list_pu, lst_del=one_dispatch_list_del)
+    for j in listofloadselements:
+        dct_load = onedisp_elem_to_dict(j)
+        one_dispatch_list.append(dct_load)
+        # print(dct_load)
 
-    '''for i in one_dispatch_list_pu:
-        print(i)
+    for k in one_dispatch_list:
+        k['pu_loc'] = get_address_from_link_selenium(k['lnk'])[0]
+        k['del_loc'] = get_address_from_link_selenium(k['lnk'])[1]
+        # print(pu)
+        # print(deliv)
+
+    one_disp_sort_pu_del(lst=one_dispatch_list, lst_pu=one_dispatch_list_pu, lst_del=one_dispatch_list_del)
+
+    # for i in one_dispatch_list_pu:
+    #     print(i)
     print(len(one_dispatch_list_pu))
-
-    for i in one_dispatch_list_del:
-        print(i)
-    print(len(one_dispatch_list_del))'''
-
-
-    # filling_sheet(sheet_name='1disp_pu', f=open_file(), load_list=one_dispatch_list_pu)
+    # for i in one_dispatch_list_del:
+    #     print(i)
+    print(len(one_dispatch_list_del))
 
 
-
-
-    '''new task
-    def get link
-    requests
-    session
-    login
-    get address'''
-
+    filling_sheet(sheet_name='1disp_pu', f=open_file(), load_list=one_dispatch_list_pu)
